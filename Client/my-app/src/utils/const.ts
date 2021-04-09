1cefbbb68b3d4eaa8ad1178dd84eab0ea1215f19
Ivan <ik0077ik25@gmail.com>
2021-04-09 11:43:48 +0300

    Start

--- Client/my-app/src/utils/const.ts
+++
@@ -1,5 +0,0 @@
-export const REGISTER_ROUTE = '/registration';
-export const LOGIN_ROUTE = '/login';
-export const PROGRAM_LIST_ROUTE = '/programList';
-export const USER_PROGRAMS_ROUTE = '/userProgramsList';
-export const ADMIN_PROGRAMS_ROUTE = '/adminProgramsList';
